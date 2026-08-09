0dc778f1a0783c984455460320dbc979b6c36b84
isac-deoanes <isacdeodanes@gmail.com>
2026-08-09 10:40:53 -0600

    se creo el js menu para la logica del menu y el responsive

---
+++ public/js/menu.js
@@ -0,0 +1,18 @@
+const menuToggle = document.getElementById('menuToggle');
+const navMenu = document.getElementById('navMenu');
+
+menuToggle.addEventListener('click', () => {
+
+    navMenu.classList.toggle('active');
+
+    if (navMenu.classList.contains('active')) {
+
+        menuToggle.textContent = '✕';
+
+    } else {
+
+        menuToggle.textContent = '☰';
+
+    }
+
+});
