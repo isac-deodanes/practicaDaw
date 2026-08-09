cd87935b5e5095c1a55032565c31a06bd5bd52d7
isac-deoanes <isacdeodanes@gmail.com>
2026-08-09 10:29:41 -0600

    se agregaron clases y desplazamiento de elemento h1

--- resources/views/Empleado/index.blade.php
+++ resources/views/Empleado/index.blade.php
@@ -2,58 +2,59 @@
 
 @section('content')
 
+    <h1>Empleados</h1>
     <div class="nav-usuario">
         <a class="btn-regresar" href="{{ route('empleado.create') }}">Nuevo Empleado</a>
         <a class="btn-reporte" href="{{ route('reporte.empleado') }}">Generar reporte</a>
 
     </div>
 
-     @if (session('mensaje'))
+    @if (session('mensaje'))
         <script>
             alert("{{ session('mensaje') }}");
-         
+
         </script>
-        
+
     @endif
-    <h1>Empleados</h1>
-    <table> 
-        <thead>
-            <tr>
-                <th>Nombre</th>
-                <th>Apellido</th>
-                <th>Correo</th>
-                <th>DUI</th>
-                <th>Telefono</th>
-                <th>Salario</th>
-                <th>Area</th>
- 
+    <div class="table-responsive">
+        <table>
+            <thead>
+                <tr>
+                    <th>Nombre</th>
+                    <th>Apellido</th>
+                    <th>Correo</th>
+                    <th>DUI</th>
+                    <th>Telefono</th>
+                    <th>Salario</th>
+                    <th>Area</th>
+                    <th>Acciones</th>
+                </tr>
+            </thead>
+            <tbody>
+                @foreach ($empleados as $empleado)
+                    <tr>
+                        <td>{{ $empleado->nombre }}</td>
+                        <td>{{ $empleado->apellido }}</td>
+                        <td>{{ $empleado->correo }}</td>
+                        <td>{{ $empleado->dui }}</td>
+                        <td>{{ $empleado->telefono }}</td>
+                        <td>{{ $empleado->salario }}</td>
+                        <td>{{ $empleado->area }}</td>
 
-                <th>Acciones</th>
-            </tr>
-        </thead>
-        <tbody>
-        @foreach ($empleados as $empleado)
-            <tr>
-                <td>{{ $empleado->nombre }}</td>
-                <td>{{ $empleado->apellido }}</td>
-                <td>{{ $empleado->correo }}</td>
-                <td>{{ $empleado->dui }}</td>
-                <td>{{ $empleado->telefono }}</td>
-                <td>{{ $empleado->salario }}</td>
-                <td>{{ $empleado->area }}</td>
+                        <td>
+                            <a class="boton-edit" href="{{ route('empleado.edit', $empleado->id) }}">Editar</a>
+                            <form class="form-boton-delete" action="{{ route('empleado.destroy', $empleado->id) }}"
+                                method="POST" style="display:inline">
+                                @csrf
+                                @method('DELETE')
+                                <button type="submit" onclick="return confirm('¿Seguro de eliminar?')">Eliminar</button>
+                            </form>
+                        </td>
+                    </tr>
+                @endforeach
+            </tbody>
+        </table>
+    </div>
 
-                <td>
-                    <a class="boton-edit" href="{{ route('empleado.edit', $empleado->id) }}">Editar</a>
-                    <form  class="form-boton-delete" action="{{ route('empleado.destroy', $empleado->id) }}" method="POST" style="display:inline">
-                        @csrf
-                        @method('DELETE')
-                        <button type="submit" onclick="return confirm('¿Seguro de eliminar?')">Eliminar</button>
-                    </form>
-                </td>
-            </tr>
-        @endforeach
-        </tbody>
-    </table>
-    
     {{ $empleados->links() }}
 @endsection
