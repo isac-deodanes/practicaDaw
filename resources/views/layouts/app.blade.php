b46e257370488a0e0f362474655aa9b76e172336
isac-deoanes <isacdeodanes@gmail.com>
2026-08-09 10:26:47 -0600

    se movieron los estilos a otroarchivo

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -3,230 +3,66 @@
 
 <head>
     <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Gestión de empleados</title>
-    <style>
-        body {
-            display: flex;
-            justify-content: flex-start;
-            flex-direction: column;
-            align-items: center;
-            margin: 0;
-            background-color: rgb(224, 222, 222);
-            font-family: Arial, sans-serif;
-            color: #4b4b4b;
-        }
+    <link rel="stylesheet" href="{{ asset('css/estilos-globales.css') }}">
+    <link rel="stylesheet" href="{{ asset('css/menu.css') }}">
 
-
-        .container {
-            width: 90%;
-            text-align: center;
-            background: #2f3640;
-            padding: 20px;
-            border-radius: 10px;
-            box-shadow: 0px 0px 10px rgba(0, 0, 0, 0.3);
-        }
-
-        table {
-            width: 75%;
-            border-collapse: collapse;
-            /* margin: auto; */
-            background: #fff;
-            box-shadow: 0px 0px 10px rgba(0, 0, 0, 0.1);
-            border-radius: 10px;
-            overflow: hidden;
-        }
-
-        th,
-        td {
-            padding: 15px;
-            text-align: center;
-            border-bottom: 1px solid #ddd;
-            color: #2c3e50;
-        }
-
-        th {
-            background-color: rgb(106, 171, 255);
-            /* Nuevo color para encabezados */
-            color: white;
-            text-transform: uppercase;
-        }
-
-        tr:hover {
-            background-color: rgba(120, 194, 255, 0.18);
-        }
-
-        a {
-            text-decoration: none;
-            color: #4b4b4b;
-
-            font-weight: bold;
-            padding: 0px 30px;
-        }
-
-        button {
-            background-color: #ff6b6b;
-            color: white;
-            border: none;
-            padding: 8px 16px;
-            border-radius: 5px;
-            margin: 35px 10px 10px;
-            cursor: pointer;
-        }
-
-        .boton-edit {
-            background-color: rgba(1, 81, 255, 0.4);
-            color: white;
-            border: none;
-            padding: 8px 16px;
-            border-radius: 5px;
-            cursor: pointer;
-        }
-
-        button:hover {
-            background-color: #e55039;
-        }
-
-        .boton-edit:hover {
-            background-color: rgba(21, 77, 200, 0.63);
-
-        }
-
-        .nav {
-            display: flex;
-            background-color: rgb(215 215 215);
-            justify-content: center;
-            align-items: center;
-            width: 100%;
-            padding: 50px 0px;
-            flex-direction: column-reverse;
-            margin-bottom: 50px;
-            box-shadow: 0px 7px 10px rgba(0, 0, 0, 0.1);
-        }
-
-        .navegacion {
-            width: 100%;
-            text-align: center;
-            font-size: 23px;
-        }
-
-        .cont-perfil {
-            width: 100%;
-            display: flex;
-            justify-content: flex-end;
-            /* border: 1px solid rgb(255, 255, 255); */
-        }
-
-        .perfil {
-            width: 50px;
-            height: 50px;
-            background-color: rgb(181, 181, 182);
-            border-radius: 50%;
-            margin-right: 80px;
-        }
-
-        .nav-usuario {
-            display: flex;
-            justify-content: space-between;
-            width: 95%;
-            margin-bottom: 55px;
-
-        }
-
-        form {
-            background: #fefefe;
-            padding: 47px;
-            border-radius: 10px;
-            box-shadow: 5px 7px 8px 2px rgb(195 195 195 / 10%);
-            text-align: left;
-            width: 45%;
-        }
-
-        .form-boton-delete {
-            background: transparent;
-            padding: 0px;
-            border-radius: 0px;
-            box-shadow: 0px;
-            text-align: center;
-            width: 0;
-        }
-
-        label {
-            display: block;
-            margin-top: 10px;
-            font-weight: bold;
-            color: #9a9c9c;
-        }
-
-        input,
-        select {
-            width: 100%;
-            padding: 8px;
-            margin-top: 5px;
-            margin-bottom: 15px;
-            border: 1px solid #ccc;
-            border-radius: 5px;
-            box-sizing: border-box;
-        }
-
-        select {
-            margin-bottom: 20px;
-        }
-
-        main {
-            padding: 20px;
-            width: 90%;
-            /* height: 58vh; */
-            display: flex;
-            flex-direction: column;
-            align-items: center;
-
-        }
-
-        .btn-regresar,
-        .btn-reporte {
-            display: inline-block;
-            padding-left: 0px;
-            border-radius: 5px;
-            padding: 10px;
-
-        }
-
-        .btn-regresar {
-            background-color: rgb(24, 93, 255);
-            color: #ebdbdb;
-
-        }
-
-        .btn-reporte {
-            background-color: red;
-            color: #ebdbdb;
-
-        }
-
-        footer {
-            display: flex;
-            background-color: rgb(224, 222, 222);
-
-
-            align-items: center;
-            justify-content: center;
-            width: 100%;
-            margin-top: 300px;
-            padding: 50px 0px;
-        }
-    </style>
 </head>
 
 <body>
-    <nav class="nav">
-        <div class="navegacion">
-            <a href="{{ route('prueba') }}">INICIO</a>
-            <a href="{{ route('empleado.index') }}">EMPLEADOS</a>
-            <a href="{{ route('producto.index') }}">PRODUCTOS</a>
-            <a href="{{ route('proveedor.index') }}">PROVEEDORES</a>
-        </div>
-        <div class="cont-perfil">
-            <div class="perfil"></div>
+    <nav class="navbar">
+        <div class="nav-container">
+            <div class="brand">
+                <a href="{{ route('inicio.index') }}">
+                    <img src="{{ asset('laravel.svg') }}" alt="img" width="45">
+                </a>
+            </div>
+            <button class="menu-toggle" id="menuToggle" aria-label="Abrir menu">
+                ☰
+            </button>
+
+            <div class="nav-menu" id="navMenu">
+                <a href="{{ route('inicio.index') }}">INICIO</a>
+                <a href="{{ route('empleado.index') }}">EMPLEADOS</a>
+                <a href="{{ route('producto.index') }}">PRODUCTOS</a>
+                <a href="{{ route('proveedor.index') }}">PROVEEDORES</a>
+                <!-- Perfil -->
+                <div id="" class="mobile-profile">
+                    MI PERFIL
+                     {{-- <div class="card-perfil">
+                        <div class="cont-perfil-foto">
+                            <div class="perfil-foto">foto</div>
+                        </div>
+                        <div id="rol">Administrador</div>
+                        <div id="name">Nombre:</div>
+                        <div id="gmail">Email:</div>
+                        <div class="btn-cerrar-session">
+                            <button id="logout">Cerrar session</button>
+                        </div>
+
+                    </div> --}}
+                </div>
+                <a href="#" class="mobile-logout">
+                    CERRAR SESION
+                </a>
+            </div>
+            <div id="cont-perfil">
+                <div id="perfil">
+                    <div class="card-perfil">
+                        <div class="cont-perfil-foto">
+                            <div class="perfil-foto">foto</div>
+                        </div>
+                        <div id="rol">Administrador</div>
+                        <div id="name">Nombre: {{Auth::user()}}</div>
+                        <div id="gmail">Email:</div>
+                        <div class="btn-cerrar-session">
+                            <button id="logout">Cerrar session</button>
+                        </div>
+
+                    </div>
+                </div>
+            </div>
         </div>
     </nav>
     <main>
@@ -236,6 +72,8 @@
         <p>&copy; 2026 Portal de proveedores. Todos los derechos reservados.</p>
     </footer>
 
+    <script src="{{asset('js/perfil.js') }}"></script>
+    <script src="{{asset('js/menu.js') }}"></script>
 </body>
 
 </html>
