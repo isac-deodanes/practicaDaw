6be945cb1f93ea6ea1840b569ca77dd65fdf626f
isac-deoanes <isacdeodanes@gmail.com>
2026-08-09 10:39:25 -0600

    se creo el archivo para mantener la logica de la grafica

---
+++ public/js/grafica.js
@@ -0,0 +1,46 @@
+const grafica = document.getElementById("grafica-dashboard")
+
+new Chart(grafica,{
+    type: 'bar',
+
+    data: {
+        labels: [
+            'Empleados',
+            'Productos',
+            'Proveedores'
+        ],
+
+        datasets: [{
+            label: 'Cantidad de registros',
+            data: [
+                empleados,
+                productos,
+                proveedores
+            ],
+            backgroundColor: [
+                '#4e73df',
+                '#1cc88a',
+                '#f6c23e'
+            ],
+            borderWidth: 1
+        }]
+    },
+
+    options: {
+        responsive: true,
+        maintainAspectRatio: false,
+
+        plugins: {
+            title: {
+                display: true,
+                text: 'Registros del Sistema'
+            }
+        },
+
+        scales: {
+            y: {
+                beginAtZero: true
+            }
+        }
+    }
+});
