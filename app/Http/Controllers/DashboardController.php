4548d256cc184c19755b2d70d6617c40ff37db41
isac-deoanes <isacdeodanes@gmail.com>
2026-08-09 11:05:13 -0600

    logica  de la grafica y los datos que se ingresan por ultima ves

---
+++ app/Http/Controllers/DashboardController.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\Empleado;
+use App\Models\proveedores;
+use App\Models\producto;
+
+class DashboardController extends Controller
+{
+    //
+
+    public function index(){
+        $totalEmpleado = Empleado::count();
+        $totalProveedores = proveedores::count();
+        $totalProductos = producto::count();
+
+        $ultimoEmpleado = Empleado::orderBy('id', 'desc')->first();
+        $ultimoProveedor = proveedores::orderBy('id', 'desc')->first();
+        $ultimoProducto = producto::orderBy('id', 'desc')->first();
+
+
+        return view('inicio',compact(
+    
+            'totalEmpleado',
+            'totalProveedores',
+            'totalProductos',
+            'ultimoEmpleado',
+            'ultimoProveedor',
+            'ultimoProducto'
+        ));
+    }
+}
