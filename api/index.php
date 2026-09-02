4454ca5f09e95d048f2eb455c54c39a4236f2576
isac-deoanes <isacdeodanes@gmail.com>
2026-09-01 20:11:36 -0600

    fix: configurando variables de entorno

--- api/index.php
+++ api/index.php
@@ -1,13 +1,28 @@
 <?php
 
-// 1. Crear directorios temporales en /tmp
+// 1. Forzar variables en todos los superglobales
+putenv('CACHE_DRIVER=array');
+putenv('CACHE_STORE=array');
+putenv('SESSION_DRIVER=cookie');
+putenv('LOG_CHANNEL=stderr');
+
+$_ENV['CACHE_DRIVER'] = 'array';
+$_ENV['CACHE_STORE'] = 'array';
+$_ENV['SESSION_DRIVER'] = 'cookie';
+$_ENV['LOG_CHANNEL'] = 'stderr';
+
+$_SERVER['CACHE_DRIVER'] = 'array';
+$_SERVER['CACHE_STORE'] = 'array';
+$_SERVER['SESSION_DRIVER'] = 'cookie';
+$_SERVER['LOG_CHANNEL'] = 'stderr';
+
+// 2. Creación de directorios en /tmp
 $storageDirs = [
     '/tmp/storage/app',
     '/tmp/storage/framework/cache/data',
     '/tmp/storage/framework/sessions',
     '/tmp/storage/framework/views',
     '/tmp/storage/logs',
-    '/tmp/bootstrap/cache',
 ];
 
 foreach ($storageDirs as $dir) {
@@ -16,17 +31,16 @@ foreach ($storageDirs as $dir) {
     }
 }
 
-// 2. Forzar rutas de almacenamiento
 putenv('APP_STORAGE=/tmp/storage');
 putenv('VIEW_COMPILED_PATH=/tmp/storage/framework/views');
 
+// 3. Autoload y arranque
 require __DIR__ . '/../vendor/autoload.php';
 
 $app = require_once __DIR__ . '/../bootstrap/app.php';
 
 $app->useStoragePath('/tmp/storage');
 
-// 3. Ejecutar petición HTTP
 $kernel = $app->make(Illuminate\Contracts\Http\Kernel::class);
 
 $response = $kernel->handle(
