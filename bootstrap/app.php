ba356be724302bb01c784aa4844a64cca047ede5
isac-deoanes <isacdeodanes@gmail.com>
2026-09-02 21:35:58 -0600

    fix: trust all proxies and enforce secure cookies for csrf validation

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -12,6 +12,8 @@ return Application::configure(basePath: dirname(__DIR__))
     )
     ->withMiddleware(function (Middleware $middleware) {
         //
+        $middleware->trustProxies(at: '*');
+
         $middleware->alias([
             'role' => \App\Http\Middleware\RoleMiddleware::class,
         ]);
